8470abf63a39caea227d609b8e0e9910c428253d
junara <jun5araki@gmail.com>
2023-05-22 00:09:11 +0900

    first commit

---
+++ src/components/SearchPageFooter.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer class="footer">
+    <div>
+      ソースコード:
+      <a href="https://github.com/junara/vue-fuchu-fee-search">GitHub</a>
+    </div>
+    <div>
+      <span>東京都府中市が</span>
+      <a
+        href="https://www.city.fuchu.tokyo.jp/kurashi/gomirisaikuru/dashikata/sodaigomi/sodaigomidasikata.html"
+        >提供しているデータ</a
+      >
+      <span>を改変して使用しています。</span>
+    </div>
+  </footer>
+</template>
+<script setup></script>
+<style scoped>
+.footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 3em;
+  background-color: #f5f5f5;
+  border-top: 1px solid #e5e5e5;
+  padding: 1em;
+  text-align: center;
+}
+</style>
--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import SearchPageTitle from '@/components/SearchPageTitle.vue';
 import SearchPageBody from '@/components/SearchPageBody.vue';
+import SearchPageFooter from '@/components/SearchPageFooter.vue';
 </script>
 <template>
   <div>
     <SearchPageTitle />
-      <a href="https://www.city.fuchu.tokyo.jp/kurashi/gomirisaikuru/dashikata/sodaigomi/sodaigomidasikata.html"> 東京都府中市が上記で提供しているデータを改変して使用しています。</a>
     <SearchPageBody />
+    <SearchPageFooter />
   </div>
 </template>
